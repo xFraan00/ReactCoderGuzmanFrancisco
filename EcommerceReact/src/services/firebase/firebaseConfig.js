870ab03b71f9e684723a336893d36a9a76f52dbe
Francisco Guzman <guzman.franciisco@gmail.com>
2024-02-09 16:45:28 -0300

    volvi atras con las credenciales debido a un error

--- EcommerceReact/src/services/firebase/firebaseConfig.js
+++ EcommerceReact/src/services/firebase/firebaseConfig.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyAYAu4eb5-s-cQTgiS1esXEy4edKZZbVNU",
+  authDomain: "reactappguzmanfrancisco.firebaseapp.com",
+  projectId: "reactappguzmanfrancisco",
+  storageBucket: "reactappguzmanfrancisco.appspot.com",
+  messagingSenderId: "1057037211008",
+  appId: "1:1057037211008:web:44cb4d6f95385977c15d65"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = getFirestore(app)
